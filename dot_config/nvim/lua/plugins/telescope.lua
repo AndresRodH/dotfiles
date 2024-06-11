fcf1c68f8cb0647299d74dcee07958fbd1640161
Andres Rodriguez <16929001+AndresRodH@users.noreply.github.com>
2024-06-11 10:37:33 -0500

    Update .config/nvim/lua/plugins/telescope.lua

--- dot_config/nvim/lua/plugins/telescope.lua
+++ dot_config/nvim/lua/plugins/telescope.lua
@@ -6,22 +6,22 @@ return {
 		-- enable finding hidden/dot files
 		{
 			"<leader><leader>",
-			Util.telescope("files", { hidden = true, no_ignore = false }),
+			Util.pick("files", { hidden = true, no_ignore = false }),
 			desc = "Find files (root dir)",
 		},
 		{
 			"<leader>ff",
-			Util.telescope("files", { hidden = true, no_ignore = false }),
+			Util.pick("files", { hidden = true, no_ignore = false }),
 			desc = "Find files (root dir)",
 		},
 		{
 			"<leader>fF",
-			Util.telescope("files", { cwd = false, hidden = true, no_ignore = false }),
+			Util.pick("files", { cwd = nil, hidden = true, no_ignore = false }),
 			desc = "Find files (cwd)",
 		},
 		{
 			"<leader>fF",
-			Util.telescope("files", { cwd = false, hidden = true, no_ignore = false }),
+			Util.pick("files", { cwd = nil, hidden = true, no_ignore = false }),
 			desc = "Find files (cwd)",
 		},
 	},
