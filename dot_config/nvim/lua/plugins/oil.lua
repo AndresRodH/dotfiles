03c7e090884559107ca6019984ea0f2e22294cae
Andres Rodriguez <16929001+AndresRodH@users.noreply.github.com>
2025-02-25 10:05:35 -0600

    do not use oil as the default file explorer

--- dot_config/nvim/lua/plugins/oil.lua
+++ dot_config/nvim/lua/plugins/oil.lua
@@ -3,7 +3,6 @@ return {
 		"stevearc/oil.nvim",
 		dependencies = { "nvim-tree/nvim-web-devicons" },
 		opts = {
-			default_file_explorer = true,
 			keymaps = {
 				["q"] = "actions.close",
 				["<C-h>"] = false,
