23ef6bc746333b4ba15b51a3ccdc0e34b7b46f2d
Andres Rodriguez <16929001+AndresRodH@users.noreply.github.com>
2025-02-25 10:04:55 -0600

    set up snacks explorer but keep oil around

--- dot_config/nvim/lua/plugins/oil.lua
+++ dot_config/nvim/lua/plugins/oil.lua
@@ -1,15 +1,7 @@
 return {
-	-- disable neo-tree
-	{
-		"nvim-neo-tree/neo-tree.nvim",
-		enabled = false,
-	},
 	{
 		"stevearc/oil.nvim",
 		dependencies = { "nvim-tree/nvim-web-devicons" },
-		keys = {
-			{ "<leader>e", "<cmd>Oil --float<CR>", desc = "Open file [E]xplorer" },
-		},
 		opts = {
 			default_file_explorer = true,
 			keymaps = {
