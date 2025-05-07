385a6318a4226fff45fc38805337a986f7bec3c0
Andres Rodriguez <16929001+AndresRodH@users.noreply.github.com>
2025-05-07 11:58:17 -0500

    pin mason until lazy supports mason v2

--- dot_config/nvim/lua/plugins/lsp.lua
+++ dot_config/nvim/lua/plugins/lsp.lua
@@ -1,18 +1,37 @@
 return {
-	"neovim/nvim-lspconfig",
-	opts = {
-		servers = {
-			vtsls = {
-				settings = {
-					typescript = {
-						tsserver = {
-							maxTsServerMemory = 8192,
+	{
+
+		"neovim/nvim-lspconfig",
+		opts = {
+			diagnostics = {
+				float = {
+					border = "rounded",
+				},
+			},
+			servers = {
+				vtsls = {
+					settings = {
+						typescript = {
+							tsserver = {
+								maxTsServerMemory = 8192,
+							},
 						},
 					},
 				},
+				graphql = {
+					filetypes = { "graphql", "typescriptreact", "javascriptreact", "vue" },
+				},
 			},
-			graphql = {
-				filetypes = { "graphql", "typescriptreact", "javascriptreact", "vue" },
+		},
+		-- pinning mason until the distro gets updated to support mason 2
+		{
+			{
+				"williamboman/mason.nvim",
+				version = "1.11.0",
+			},
+			{
+				"mason-org/mason-lspconfig.nvim",
+				version = "1.32.0",
 			},
 		},
 	},
