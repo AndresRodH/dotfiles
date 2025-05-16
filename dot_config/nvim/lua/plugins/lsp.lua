3c0e39b93fa3d1b4287b7f20a6bad3e671e0bfcc
Andres Rodriguez <16929001+AndresRodH@users.noreply.github.com>
2025-05-16 14:50:20 -0500

    undo Mason workaround since it's on main now

--- dot_config/nvim/lua/plugins/lsp.lua
+++ dot_config/nvim/lua/plugins/lsp.lua
@@ -1,37 +1,23 @@
 return {
-	{
-
-		"neovim/nvim-lspconfig",
-		opts = {
-			diagnostics = {
-				float = {
-					border = "rounded",
-				},
+	"neovim/nvim-lspconfig",
+	opts = {
+		diagnostics = {
+			float = {
+				border = "rounded",
 			},
-			servers = {
-				vtsls = {
-					settings = {
-						typescript = {
-							tsserver = {
-								maxTsServerMemory = 8192,
-							},
+		},
+		servers = {
+			vtsls = {
+				settings = {
+					typescript = {
+						tsserver = {
+							maxTsServerMemory = 8192,
 						},
 					},
 				},
-				graphql = {
-					filetypes = { "graphql", "typescriptreact", "javascriptreact", "vue" },
-				},
 			},
-		},
-		-- pinning mason until the distro gets updated to support mason 2
-		{
-			{
-				"williamboman/mason.nvim",
-				version = "1.11.0",
-			},
-			{
-				"mason-org/mason-lspconfig.nvim",
-				version = "1.32.0",
+			graphql = {
+				filetypes = { "graphql", "typescriptreact", "javascriptreact", "vue" },
 			},
 		},
 	},
