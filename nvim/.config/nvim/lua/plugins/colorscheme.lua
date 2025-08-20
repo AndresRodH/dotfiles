dab80e9cb9a72ad79a6634cd2f998bd68b6f30db
Andres Rodriguez <16929001+AndresRodH@users.noreply.github.com>
2025-08-20 11:45:17 -0500

    workaround catppuccin breaking change on bufferline
    see https://github.com/LazyVim/LazyVim/pull/6354

--- nvim/.config/nvim/lua/plugins/colorscheme.lua
+++ nvim/.config/nvim/lua/plugins/colorscheme.lua
@@ -5,4 +5,12 @@ return {
 			colorscheme = "catppuccin",
 		},
 	},
+	{
+		"catppuccin",
+		optional = true,
+		opts = function()
+			local bufferline = require("catppuccin.groups.integrations.bufferline")
+			bufferline.get = bufferline.get or bufferline.get_theme
+		end,
+	},
 }
